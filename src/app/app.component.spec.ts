aa8a62bd1b3488e8adef4187c94c104aee360550
Thomas Gotwig <tgotwig@gmail.com>
2020-03-05 14:23:56 +0100

    Fix AppComponent tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,37 +1,37 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
 
-// TODO: Fix tests
+import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
-    // TestBed.configureTestingModule({
-    //   imports: [
-    //     RouterTestingModule
-    //   ],
-    //   declarations: [
-    //     AppComponent
-    //   ],
-    // }).compileComponents();
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
   }));
 
   it('should create the app', () => {
-    // const fixture = TestBed.createComponent(AppComponent);
-    // const app = fixture.componentInstance;
-    // expect(app).toBeTruthy();
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
   });
 
   it(`should have as title 'todos-demo-frontend'`, () => {
-    // const fixture = TestBed.createComponent(AppComponent);
-    // const app = fixture.componentInstance;
-    // expect(app.title).toEqual('todos-demo-frontend');
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('todos-demo-frontend');
   });
 
-  it('should render title', () => {
-    // const fixture = TestBed.createComponent(AppComponent);
-    // fixture.detectChanges();
-    // const compiled = fixture.nativeElement;
-    // expect(compiled.querySelector('.content span').textContent).toContain('todos-demo-frontend app is running!');
+  it(`should have as activeTab 'todos'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.activeTab).toEqual('todos');
   });
 });
