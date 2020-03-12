71f4c2932d7d24e698829c83ee4c9299b6a90b73
Thomas Gotwig <tgotwig@gmail.com>
2020-03-12 23:35:06 +0100

    Add Jasmine tests for todos-component

--- src/app/todos/todos.component.spec.ts
+++ src/app/todos/todos.component.spec.ts
@@ -1,26 +1,50 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { TodosComponent } from './todos.component';
-
-// TODO: Fix tests
+import { DataService } from '../#services/data.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('TodosComponent', () => {
   let component: TodosComponent;
   let fixture: ComponentFixture<TodosComponent>;
+  let dataService: DataService;
 
   beforeEach(async(() => {
-    // TestBed.configureTestingModule({
-    //   declarations: [ TodosComponent ]
-    // })
-    // .compileComponents();
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientModule
+      ],
+      declarations: [
+        TodosComponent
+      ],
+    }).compileComponents();
+    dataService = TestBed.inject(DataService);
   }));
 
   beforeEach(() => {
-    // fixture = TestBed.createComponent(TodosComponent);
-    // component = fixture.componentInstance;
-    // fixture.detectChanges();
+    fixture = TestBed.createComponent(TodosComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+    dataService.todos = [];
   });
 
   it('should create', () => {
-    // expect(component).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('should display welcome message', () => {
+    fixture = TestBed.createComponent(TodosComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('#noTodosInfo').textContent)
+      .toContain('You have no todos 🙂 Enjoy 🏝 Or create new ones 👉');
+  });
+
+  it('should display a todo item', () => {
+    dataService.todos.push({ id: 'testId', text: 'testText' });
+    fixture = TestBed.createComponent(TodosComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelectorAll('.todoWrapper').length)
+      .toBeGreaterThan(0);
   });
 });
